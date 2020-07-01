794d843ac81faa78602a1e6d924f8df338601ee3
lane-c-wagner <lane.c.wagner@gmail.com>
2020-06-30 21:21:09 -0600

    playground on mobile

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,27 +1,43 @@
 <template>
   <div id="container">
     <div id="header">
-      <BlockButton
-        id="btn"
-        :click="() => {
-          if (this.$store.getters.getIsLoggedIn){
-            this.$router.push({name: 'Courses'});
-          } else {
-            this.$router.push({name: 'Login'});
-          }
-        }"
-      >
-        <FontAwesomeIcon
-          icon="undo"
-        />
-        To Courses
-      </BlockButton>
-      <span>
-        <FontAwesomeIcon
-          icon="cubes"
-        />
-        Qvault Playground
-      </span>
+      <div>
+        <BlockButton
+          id="btn"
+          :click="() => {
+            if (this.$store.getters.getIsLoggedIn){
+              this.$router.push({name: 'Courses'});
+            } else {
+              this.$router.push({name: 'Login'});
+            }
+          }"
+        >
+          <FontAwesomeIcon
+            icon="undo"
+          />
+          To Courses
+        </BlockButton>
+      </div>
+
+      <div id="title-container">
+        <span id="title">
+          <FontAwesomeIcon
+            icon="cubes"
+          />
+          Qvault Playground
+        </span>
+        <span id="sub-title">
+          Run {{ lang }} in your browser with web assembly (desktop only)
+        </span>
+      </div>
+      <div>
+        <a
+          href="https://qvault.io"
+          target="_blank"
+        >
+          About
+        </a>
+      </div>
     </div>
     <CodeEditor
       id="editor"
@@ -46,7 +62,10 @@ export default {
   },
   computed: {
     lang(){
-      return this.$route.params.lang;
+      if (this.$route.params.lang === 'go'){
+        return 'Go';
+      }
+      return 'unknown';
     }
   },
   mounted(){
@@ -85,22 +104,40 @@ func main(){
   }
 
   #header {
-    text-align: center;
+    padding: 10px;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    align-items: center;
 
-    span {
-      font-size: 2.5em;
-      line-height: 2.5em;
-      color: $gray-lightest;
+    div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      flex: 1;
+      align-items: center;
+      min-width: 250px;
+    }
+
+    #title-container{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+    
+      #title{
+        font-size: 2.5em;
+        color: $gray-lightest;
+      }
+
+      #sub-title{
+        font-size: 1em;
+        color: $purple-lighter;
+      }
     }
 
     #btn {
-      font-size: 1.2em;
-      padding: 15px;
-      max-width: 200px;
-      left: 0;
-      top: 0;
-      margin: 20px;
-      position: absolute;
+      font-size: 1.5em;
     }
   }
 }
