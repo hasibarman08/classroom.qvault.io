1f5462f0cf65eea8031cb4462b390f4682a8a92e
lane-c-wagner <lane.c.wagner@gmail.com>
2020-07-19 15:33:11 -0600

    switch langs

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="container">
     <TopNav :title="`${displayLang} Playground`" />
+
+    <div class="langs">
+      <a
+        v-for="(langKey, i) of langsArray"
+        :key="i"
+        :href="`/playground/${langKey}`"
+      >
+        {{ langs[langKey].displayName }}
+      </a>
+    </div>
+
     <CodeEditor
       id="editor"
       ref="codeEditor"
@@ -22,18 +33,23 @@ export default {
   },
   data(){
     return {
-      lang: this.$route.params.lang
+      lang: this.$route.params.lang,
+      langs: {
+        go:{
+          displayName: 'Go WASM'
+        },
+        js:{
+          displayName: 'JavaScript'
+        }
+      }
     };
   },
   computed: {
     displayLang(){
-      if (this.lang === 'go'){
-        return 'Go WASM';
-      }
-      if (this.lang === 'js'){
-        return 'JavaScript';
-      }
-      return 'unknown';
+      return this.langs[this.lang].displayName;
+    },
+    langsArray(){
+      return Object.keys(this.langs);
     }
   },
   mounted(){
@@ -54,6 +70,12 @@ import "fmt"
 func main(){
     fmt.Println("hello world")
 }
+
+// code is compiled to WASM
+// on our servers then
+// executes on your machine
+// read about it on our blog:
+// https://qvault.io/2020/07/01/running-go-in-the-browser-with-web-assembly-wasm/
     `);
         return;
       }
@@ -76,6 +98,16 @@ func main(){
   justify-content: flex-end;
   background-color: $gray-darkest;
   height: 100%;
+
+  .langs{
+    display: flex;
+    flex-direction: row;
+    padding: 1em;
+    
+    a {
+      padding-left: 10px;
+    }
+  }
 
   #editor {
     flex: 1;
